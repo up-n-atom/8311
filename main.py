ef4a94c53ba4f869689b0553fc771471b019eb3c
MrLenin <909621+MrLenin@users.noreply.github.com>
2023-08-26 16:25:49 -0400

    Convert G-440G-A and DFP-34X-2C2 to use device template. Improve credential handling.

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ def define_env(env):
 
     @env.macro
     def credentials(type, cred_list):
-        text = "=== \"{0}\"\n    | Username            | Password            | Privilege           |\n    | ------------------- | ------------------- | ------------------- |"
+        buffer = ""
+        table_templ = "\n    | {0} | {1} | {2}"
+        tab_templ = "=== \"{0}\"\n    | Username | Password | {1}\n    | ---- | ---- | {2}"
+        priv = False
         for cred in cred_list:
-            username = cred['username']
-            password = cred['password']
+            username = cred.get('username')
+            password = cred.get('password')
             privilege = cred.get('privilege')
-            text += "\n    | " + username + " | " + password + " | "
             if privilege is not None:
-                text += privilege + " |"
+                priv = True
+                buffer += table_templ.format(username, password, privilege) + " |"
             else:
-                text += "Unknown |"
-        return text.format(type)
+                buffer += table_templ.format(username, password, "")
+        if priv:
+            buffer = tab_templ.format(type, "Privilege |", "---- |") + buffer
+        else:
+            buffer = tab_templ.format(type, "", "") + buffer
+        return buffer
 
     @env.macro
     def iterate_specifications(onu):
@@ -24,7 +31,7 @@ def define_env(env):
             first = True
             for spec in specifications:
                 if first:
-                    text += "| " + spec[0] + " | " + spec[1] + " |\n| ------ | ------ |"
+                    text += "| " + spec[0] + " | " + spec[1] + " |\n| ---- | ---- |"
                     first = False
                 else:
                     text += "\n| " + spec[0] + " | " + spec[1] + " |"
